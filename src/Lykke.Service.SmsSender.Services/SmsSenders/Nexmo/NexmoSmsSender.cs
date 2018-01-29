d3ede0d84848cdba82f5643e1a3bb6938d147ffd
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-01-30 01:09:32 +0300

    added logs for error responses

--- src/Lykke.Service.SmsSender.Services/SmsSenders/Nexmo/NexmoSmsSender.cs
+++ src/Lykke.Service.SmsSender.Services/SmsSenders/Nexmo/NexmoSmsSender.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Common;
+using Common.Log;
 using Flurl.Http;
 using Lykke.Service.SmsSender.Core.Services;
 using Lykke.Service.SmsSender.Core.Settings.ServiceSettings.SenderSettings;
@@ -10,14 +12,17 @@ namespace Lykke.Service.SmsSender.Services.SmsSenders.Nexmo
     {
         private readonly string _baseUrl;
         private readonly ProviderSettings _settings;
+        private readonly ILog _log;
         private const string BaseUrl = "https://rest.nexmo.com";
         
         public NexmoSmsSender(
             string baseUrl,
-            ProviderSettings settings)
+            ProviderSettings settings,
+            ILog log)
         {
             _baseUrl = baseUrl;
             _settings = settings;
+            _log = log;
         }
 
         public async Task<string> SendSmsAsync(string phone, string message, string countryCode)
@@ -30,12 +35,29 @@ namespace Lykke.Service.SmsSender.Services.SmsSenders.Nexmo
                     text = message,
                     api_key = _settings.ApiKey,
                     api_secret = _settings.ApiSecret,
-                    callback = $"{_baseUrl}/callback/nexmo",
-                    
+                    callback = $"{_baseUrl}/callback/nexmo"
                 }).ReceiveJson<NexmoResponse>();
 
             if (response.MessagesCount > 0)
+            {
+                var errors = response.Messages
+                    .Where(item => item.Status != NexmoStatus.Ok)
+                    .Select(item => new
+                        {
+                            Phone = item.To.SanitizePhone(),
+                            item.MessagePrice,
+                            item.RemainingBalance,
+                            item.Status
+                        })
+                    .ToList();
+
+                if (errors.Any())
+                {
+                    _log.WriteWarning(nameof(SendSmsAsync), errors, "nexmo error messages");
+                }
+                
                 return response.Messages.FirstOrDefault(item => item.Status == NexmoStatus.Ok)?.MessageId;
+            }
 
             return null;
         }
--- src/Lykke.Service.SmsSender.Services/SmsSenders/Twilio/TwilioSmsSender.cs
+++ src/Lykke.Service.SmsSender.Services/SmsSenders/Twilio/TwilioSmsSender.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Common;
+using Common.Log;
 using Flurl;
 using Flurl.Http;
 using Lykke.Service.SmsSender.Core.Services;
@@ -10,14 +12,17 @@ namespace Lykke.Service.SmsSender.Services.SmsSenders.Twilio
     {
         private readonly string _baseUrl;
         private readonly ProviderSettings _settings;
+        private readonly ILog _log;
         private const string BaseApiUrl = "https://api.twilio.com/2010-04-01";
 
         public TwilioSmsSender(
             string baseUrl,
-            ProviderSettings settings)
+            ProviderSettings settings,
+            ILog log)
         {
             _baseUrl = baseUrl;
             _settings = settings;
+            _log = log;
         }
 
         public async Task<string> SendSmsAsync(string phone, string message, string countryCode)
@@ -32,8 +37,21 @@ namespace Lykke.Service.SmsSender.Services.SmsSenders.Twilio
                     StatusCallback = $"{_baseUrl}/callback/twilio"
                 }).ReceiveJson<TwilioResponse>();
 
-            if (string.IsNullOrEmpty(response.ErrorMessage))
+            if (!string.IsNullOrEmpty(response.ErrorMessage))
+            {
+                _log.WriteWarning(nameof(SendSmsAsync), new
+                {
+                    response.Sid,
+                    Phone = response.To.SanitizePhone(),
+                    response.Status,
+                    response.ErrorCode,
+                    response.ErrorMessage
+                }, "twilio error response");
+            }
+            else
+            {
                 return response.Sid;
+            }
 
             return null;
         }
