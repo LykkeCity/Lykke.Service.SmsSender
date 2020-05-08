546328b3218aaae42d47e7e0c9d8557029fa7cd5
Dmitry Samodov <samodov.dmitry@gmail.com>
2020-05-08 15:22:58 +0300

    retry sending in Nexmo sms sender

--- src/Lykke.Service.SmsSender.Services/SmsSenders/Nexmo/NexmoSmsSender.cs
+++ src/Lykke.Service.SmsSender.Services/SmsSenders/Nexmo/NexmoSmsSender.cs
@@ -28,57 +28,61 @@ namespace Lykke.Service.SmsSender.Services.SmsSenders.Nexmo
 
         public async Task<string> SendSmsAsync(string phone, string message, string countryCode)
         {
-            try
+            int index = 0;
+            while (++index <= 5)
             {
-                var sw = new Stopwatch();
-                _log.WriteInfo(nameof(SendSmsAsync), new {Phone = phone.SanitizePhone(), CountryCode = countryCode},
-                    $"Sending sms to nexmo endpoint {_settings.BaseUrl}/sms/json");
-
-                sw.Start();
-                var response = await $"{_settings.BaseUrl}/sms/json"
-                    .WithTimeout(10)
-                    .PostUrlEncodedAsync(new
-                    {
-                        to = phone,
-                        from = _settings.GetFrom(countryCode),
-                        text = message,
-                        api_key = _settings.ApiKey,
-                        api_secret = _settings.ApiSecret,
-                        callback = $"{_baseUrl}/callback/nexmo"
-                    }).ReceiveJson<NexmoResponse>();
-
-                sw.Stop();
-
-                _log.WriteInfo(nameof(SendSmsAsync), new {messagesCount = response.MessagesCount, ElapsedMsec = sw.ElapsedMilliseconds },
-                    $"Sms has been sent to nexmo endpoint {_settings.BaseUrl}/sms/json");
-
-                if (response.MessagesCount > 0)
+                try
                 {
-                    var errors = response.Messages
-                        .Where(item => item.Status != NexmoStatus.Ok)
-                        .Select(item => new
+                    var sw = new Stopwatch();
+                    _log.WriteInfo(nameof(SendSmsAsync), new {Phone = phone.SanitizePhone(), CountryCode = countryCode},
+                        $"Sending sms to nexmo endpoint {_settings.BaseUrl}/sms/json");
+
+                    sw.Start();
+                    var response = await $"{_settings.BaseUrl}/sms/json"
+                        .WithTimeout(10)
+                        .PostUrlEncodedAsync(new
                         {
-                            Phone = phone.SanitizePhone(), item.Status, item.Error, item.RemainingBalance
-                        })
-                        .ToList();
+                            to = phone,
+                            from = _settings.GetFrom(countryCode),
+                            text = message,
+                            api_key = _settings.ApiKey,
+                            api_secret = _settings.ApiSecret,
+                            callback = $"{_baseUrl}/callback/nexmo"
+                        }).ReceiveJson<NexmoResponse>();
 
-                    if (errors.Any())
+                    sw.Stop();
+
+                    _log.WriteInfo(nameof(SendSmsAsync), new {messagesCount = response.MessagesCount, ElapsedMsec = sw.ElapsedMilliseconds },
+                        $"Sms has been sent to nexmo endpoint {_settings.BaseUrl}/sms/json");
+
+                    if (response.MessagesCount > 0)
                     {
-                        var notEnoughFunds =
-                            errors.FirstOrDefault(item => item.Status == NexmoStatus.PartnerQuotaExceeded);
+                        var errors = response.Messages
+                            .Where(item => item.Status != NexmoStatus.Ok)
+                            .Select(item => new
+                            {
+                                Phone = phone.SanitizePhone(), item.Status, item.Error, item.RemainingBalance
+                            })
+                            .ToList();
 
-                        if (notEnoughFunds != null)
-                            _log.WriteError(nameof(SendSmsAsync), notEnoughFunds);
-                        else
-                            _log.WriteWarning(nameof(SendSmsAsync), errors, "error sending sms");
+                        if (errors.Any())
+                        {
+                            var notEnoughFunds =
+                                errors.FirstOrDefault(item => item.Status == NexmoStatus.PartnerQuotaExceeded);
+
+                            if (notEnoughFunds != null)
+                                _log.WriteError(nameof(SendSmsAsync), notEnoughFunds);
+                            else
+                                _log.WriteWarning(nameof(SendSmsAsync), errors, "error sending sms");
+                        }
+
+                        return response.Messages.FirstOrDefault(item => item.Status == NexmoStatus.Ok)?.MessageId;
                     }
-
-                    return response.Messages.FirstOrDefault(item => item.Status == NexmoStatus.Ok)?.MessageId;
                 }
-            }
-            catch (Exception ex)
-            {
-                _log.WriteWarning(nameof(SendSmsAsync), ex.Message, "nexmo: error sending sms", ex);
+                catch (Exception ex)
+                {
+                    _log.WriteWarning(nameof(SendSmsAsync), ex.Message, "nexmo: error sending sms", ex);
+                }
             }
 
             return null;
