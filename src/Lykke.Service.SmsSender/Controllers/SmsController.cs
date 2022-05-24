cfc5cee13a273ebd7b875f135f1829d12e58f57b
Andrey Volkov <avolkov.dev.10x@gmail.com>
2022-05-24 12:10:16 +0300

    Fix PR notes (add Trim())

--- src/Lykke.Service.SmsSender/Controllers/SmsController.cs
+++ src/Lykke.Service.SmsSender/Controllers/SmsController.cs
@@ -54,7 +54,7 @@ namespace Lykke.Service.SmsSender.Controllers
 
             try
             {
-                _cqrsEngine.SendCommand(new ProcessSmsCommand {Message = model.Message, Phone = model.Phone.Replace(" ","")}, "sms", "sms");
+                _cqrsEngine.SendCommand(new ProcessSmsCommand {Message = model.Message, Phone = model.Phone.Replace(" ","").Trim()}, "sms", "sms");
                 return Ok();
             }
             catch (Exception ex)
